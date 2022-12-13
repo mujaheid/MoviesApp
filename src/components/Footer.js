1f351382d7a99599910da2619226a1282b4c90fc
mujaheid <98412377+mujaheid@users.noreply.github.com>
2022-12-13 22:43:53 +0530

    Add files via upload

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import "./Footer.css";
+function Footer() {
+  return (
+    <>
+      <p className="footer">
+        <small> A Newton School Project Developed By @ Syed Mujaheid</small>
+      </p>
+    </>
+  );
+}
+
+export default Footer;
